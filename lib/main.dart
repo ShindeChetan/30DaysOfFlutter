ceb6c58c9148b17636d003d815299499411e165a
Chetan Shinde <chetanshinde2230@gmail.com>
2021-02-14 14:43:28 +0530

    Day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,13 +10,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
+    // //double pi = 3.14;
+    // bool isMale = true;
+    // num temp = 30.5;
+
+    // var day = "Tuesday";
+    // const pi = 3.14;
+
     return MaterialApp(
-        home: Material(
-      child: Center(
-        child: Container(
-          child: Text("Welcome to #30DaysOfFlutter"),
-        ),
-      ),
-    ));
+      home: HomePage(),
+    );
   }
 }
