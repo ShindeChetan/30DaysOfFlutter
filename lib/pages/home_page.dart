91bf78aa5100acb4d95412c3a61f63d338964e73
Chetan Shinde <chetanshinde2230@gmail.com>
2021-02-21 18:54:43 +0530

    Day12 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
-//Day11 was about constraints and contexts
 class HomePage extends StatelessWidget {
-  int days = 30;
-  String name = "Chetan";
-
+  final int days = 30;
+  final String name = "Codepur";
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Catalog App",
-        ),
+        title: Text("Catalog App"),
       ),
-      body: Container(
-        child: Text("Welcome to $days days of flutter by $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
+        ),
       ),
       drawer: MyDrawer(),
     );
